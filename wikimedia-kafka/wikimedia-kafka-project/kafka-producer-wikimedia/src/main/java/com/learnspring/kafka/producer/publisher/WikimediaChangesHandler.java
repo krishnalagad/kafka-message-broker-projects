5a0819565177f86e65ce135761928883911d5559
Krishna Lagad <krishnalagad2@gmail.com>
2023-07-22 19:48:50 +0530

    - fix done
    - changed the version of okhttp-datasource dependency.

--- wikimedia-kafka/wikimedia-kafka-project/kafka-producer-wikimedia/src/main/java/com/learnspring/kafka/producer/publisher/WikimediaChangesHandler.java
+++ wikimedia-kafka/wikimedia-kafka-project/kafka-producer-wikimedia/src/main/java/com/learnspring/kafka/producer/publisher/WikimediaChangesHandler.java
@@ -1,7 +1,32 @@
 package com.learnspring.kafka.producer.publisher;
 
+import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.MessageEvent;
 
-public class WikimediaChangesHandler implements  {
+public class WikimediaChangesHandler implements EventHandler {
 
+    @Override
+    public void onOpen() throws Exception {
+        
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
 }

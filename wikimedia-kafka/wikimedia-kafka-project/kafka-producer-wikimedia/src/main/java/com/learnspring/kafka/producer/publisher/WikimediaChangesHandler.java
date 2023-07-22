f0a800a7821d276d83e4b42d88792a097d0e00e2
Krishna Lagad <krishnalagad2@gmail.com>
2023-07-22 20:09:04 +0530

    - dump kafka producer

--- wikimedia-kafka/wikimedia-kafka-project/kafka-producer-wikimedia/src/main/java/com/learnspring/kafka/producer/publisher/WikimediaChangesHandler.java
+++ wikimedia-kafka/wikimedia-kafka-project/kafka-producer-wikimedia/src/main/java/com/learnspring/kafka/producer/publisher/WikimediaChangesHandler.java
@@ -2,12 +2,25 @@ package com.learnspring.kafka.producer.publisher;
 
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 
 public class WikimediaChangesHandler implements EventHandler {
 
+    private Logger logger = LoggerFactory.getLogger(WikimediaChangesHandler.class);
+    private KafkaTemplate kafkaTemplate;
+    private String topic;
+
+    public WikimediaChangesHandler(KafkaTemplate kafkaTemplate, String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
     @Override
     public void onOpen() throws Exception {
-        
+
     }
 
     @Override
@@ -17,7 +30,8 @@ public class WikimediaChangesHandler implements EventHandler {
 
     @Override
     public void onMessage(String s, MessageEvent messageEvent) throws Exception {
-
+        this.logger.info(String.format("Event data -> %s", messageEvent.getData()));
+        this.kafkaTemplate.send(topic, messageEvent.getData());
     }
 
     @Override
--- wikimedia-kafka/wikimedia-kafka-project/kafka-producer-wikimedia/src/main/java/com/learnspring/kafka/producer/publisher/WikimediaChangesProducer.java
+++ wikimedia-kafka/wikimedia-kafka-project/kafka-producer-wikimedia/src/main/java/com/learnspring/kafka/producer/publisher/WikimediaChangesProducer.java
@@ -1,11 +1,16 @@
 package com.learnspring.kafka.producer.publisher;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WikimediaChangesProducer {
@@ -18,9 +23,15 @@ public class WikimediaChangesProducer {
     @Value("${kafka.topic1.name}")
     private String topic1;
 
-    public void sendMessage() {
+    public void sendMessage() throws InterruptedException {
 
         // to read real time stream data from wikimedia, we use event source.
+        EventHandler eventHandler = new WikimediaChangesHandler(this.kafkaTemplate, this.topic1);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource .Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
 
+        TimeUnit.MINUTES.sleep(10);
     }
 }

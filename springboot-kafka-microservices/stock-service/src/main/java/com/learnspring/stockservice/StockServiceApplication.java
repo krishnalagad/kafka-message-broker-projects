c4809f03a2cb5f7653a993fa120a4341c4941fd5
Krishna Lagad <krishnalagad2@gmail.com>
2023-07-24 14:04:22 +0530

    - dump all microservices project.

---
+++ springboot-kafka-microservices/stock-service/src/main/java/com/learnspring/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
---
+++ springboot-kafka-microservices/stock-service/src/test/java/com/learnspring/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

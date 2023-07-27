c366c4fd232a12452aff8a9e78b35262cfffa5a0
Krishna Lagad <krishnalagad2@gmail.com>
2023-07-27 21:01:27 +0530

    - dump email microservice

---
+++ springboot-kafka-microservices/email-service/src/main/java/com/learnspring/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,20 @@
+package com.learnspring.emailservice.kafka;
+
+import com.learnspring.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        this.logger.info(String.format("Order event received in stock service -> %s", event));
+
+        // Here we can save event data in database.
+    }
+}
